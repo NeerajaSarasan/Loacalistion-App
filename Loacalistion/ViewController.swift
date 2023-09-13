5b42a40ed1c2a837804c99b7f5e57f77e8149391
Neeraja Sarasan <neeraja.20002023@gmail.com>
2023-09-13 14:15:41 +0530

    secondcommit

--- Loacalistion/ViewController.swift
+++ Loacalistion/ViewController.swift
@@ -12,18 +12,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var segmentedCntrl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    
-    
     
     @IBAction func segmentedControl(_ sender: UISegmentedControl) {
         if segmentedCntrl.selectedSegmentIndex == 0 {
             var path = Bundle.main.path(forResource: "en", ofType: "lproj")
             var bundle = Bundle(path: path!)
-            
             label1.text = NSLocalizedString("label1Key",bundle: bundle!, comment: "")
             label2.text = NSLocalizedString("label2Key", bundle: bundle!,comment: "")
         }else {
@@ -32,7 +29,7 @@ class ViewController: UIViewController {
             label1.text = NSLocalizedString("label1Key",bundle: bundle! ,comment: "")
             label2.text = NSLocalizedString("label2Key",bundle: bundle! , comment: "")
         }
-        
     }
+    
 }
 
