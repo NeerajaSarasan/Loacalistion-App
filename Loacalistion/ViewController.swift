c42f9aefec73a87de9f4cc38ee1750fc62192e58
Neeraja Sarasan <neeraja.20002023@gmail.com>
2023-09-13 09:16:54 +0530

    Initial Commit

---
+++ Loacalistion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loacalistion
+//
+//  Created by Neeraja Sarasan on 13/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
