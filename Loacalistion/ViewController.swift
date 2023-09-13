4081d368931edfe5cecbd8e26a73a00e814d43e0
Neeraja Sarasan <neeraja.20002023@gmail.com>
2023-09-13 13:57:35 +0530

    initial commit

--- Loacalistion/ViewController.swift
+++ Loacalistion/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var segmentedCntrl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        if segmentedCntrl.selectedSegmentIndex == 0 {
+            var path = Bundle.main.path(forResource: "en", ofType: "lproj")
+            var bundle = Bundle(path: path!)
+            
+            label1.text = NSLocalizedString("label1Key",bundle: bundle!, comment: "")
+            label2.text = NSLocalizedString("label2Key", bundle: bundle!,comment: "")
+        }else {
+            var path = Bundle.main.path(forResource: "fr", ofType: "lproj")
+            var bundle = Bundle(path: path!)
+            label1.text = NSLocalizedString("label1Key",bundle: bundle! ,comment: "")
+            label2.text = NSLocalizedString("label2Key",bundle: bundle! , comment: "")
+        }
+        
+    }
 }
 
